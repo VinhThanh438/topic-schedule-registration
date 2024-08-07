071b6952097cd039733d6030d95925a8d3d3d0bb
ThanhVinh438 <vuthanhvinh438@gmail.com>
2024-08-07 15:40:33 +0700

    update generate time handler

--- src/common/utils/generate-time.ts
+++ src/common/utils/generate-time.ts
@@ -1,43 +1,70 @@
-export class GenerateTime {
-    private static type: string;
-    private static date: Date;
+import { IGenerateTime } from "./generate-time.interface";
 
-    constructor(type: string, date: Date) {
+export class GenerateTime {
+    private static modId: string;
+    private static type: string;
+    private static date: string;
+
+    constructor(modId: string, type: string, date: string) {
+        GenerateTime.modId = modId
         GenerateTime.type = type;
         GenerateTime.date = date;
     }
 
-    static generate(): { time: Date }[] {
-        const dateString = GenerateTime.date.toISOString().slice(0, 10);
-        switch (GenerateTime.type) {
+    public static formatTime(hour: number, minute: number) {
+        return `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`;
+    }
+
+    public static generate(): IGenerateTime[] {
+        const parseString = this.date.replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$3-$2-$1')
+        const convertDate = new Date(parseString);
+        switch (this.type) {
             case 'M': { // Morning
-                const result: { time: Date }[] = [];
+                const result: IGenerateTime[] = [];
                 for (let hour = 8; hour <= 11; hour++) {
                     for (let minute = 0; minute < 60; minute += 30) {
-                        const time = `${dateString}T${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:00.000+00:00`;
-                        result.push({ time: new Date(time) });
+                        const dateString:string = parseString + `T${this.formatTime(hour, minute)}:00.000Z`
+                        let startTime = Date.parse(dateString) // Unix timestamp
+                        let endTime = startTime + 1800;
+                        result.push({
+                            mod_id: this.modId,
+                            start_time: startTime,
+                            end_time: endTime
+                        });
                     }
                 }
                 return result;
             }
 
             case 'A': { // Afternoon
-                const result: { time: Date }[] = [];
+                const result: IGenerateTime[] = [];
                 for (let hour = 14; hour <= 17; hour++) {
                     for (let minute = 0; minute < 60; minute += 30) {
-                        const time = `${dateString}T${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:00.000+00:00`;
-                        result.push({ time: new Date(time) });
+                        const dateString:string = parseString + `T${this.formatTime(hour, minute)}:00.000Z`
+                        let startTime = Date.parse(dateString) // Unix timestamp
+                        let endTime = startTime + 1800;
+                        result.push({
+                            mod_id: this.modId,
+                            start_time: startTime,
+                            end_time: endTime
+                        });
                     }
                 }
                 return result;
             }
 
             case 'E': { // Evening
-                const result: { time: Date }[] = [];
+                const result: IGenerateTime[] = [];
                 for (let hour = 19; hour <= 22; hour++) {
                     for (let minute = 0; minute < 60; minute += 30) {
-                        const time = `${dateString}T${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:00.000+00:00`;
-                        result.push({ time: new Date(time) });
+                        const dateString:string = parseString + `T${this.formatTime(hour, minute)}:00.000Z`
+                        let startTime = Date.parse(dateString) // Unix timestamp
+                        let endTime = startTime + 1800;
+                        result.push({
+                            mod_id: this.modId,
+                            start_time: startTime,
+                            end_time: endTime
+                        });
                     }
                 }
                 return result;

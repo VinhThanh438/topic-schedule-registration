6494ace9efa69b0281f9d283c540f26394174109
ThanhVinh438 <vuthanhvinh438@gmail.com>
2024-08-15 17:47:16 +0700

    update code

---
+++ src/api/user/user.midleware.ts
@@ -0,0 +1,20 @@
+import logger from "@common/logger";
+import { IUserScheduling } from "@common/user/user.interface";
+import { UserService } from "@common/user/user.service";
+import { StatusCode } from "@config/status-code";
+import { NextFunction, Request, Response } from "express";
+
+export class UserMidleware {
+    static async checkRemainingLessions(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const body = req.body as any
+            const check = await UserService.checkRemainingLession(body as IUserScheduling)
+
+            if (!check) throw new Error('user has no lessions left!')
+            else next()
+        } catch (error) {
+            logger.error(error.message)
+            res.status(StatusCode.REQUEST_FORBIDDEN).json({ message: error.message })
+        }
+    }
+}

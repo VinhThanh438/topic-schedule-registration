2b686d79e228ca2de41bf758291f3db6f3248418
Thanh Vinh <vuthanhvinh438@gmail.gmailcom>
2024-08-18 12:19:28 +0700

    add auth

---
+++ src/api/auth/auth.middleware.ts
@@ -0,0 +1,57 @@
+import { RedisAdapter } from '@common/infrastructure/redis.adapter';
+import { StatusCode } from '@config/status-code';
+import { isTokenExpried, Token } from '@config/token';
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+export class AuthMidleware {
+    public static async requireAuth(req: Request, res: Response, next: NextFunction) {
+        try {
+            const accessToken = req.headers.accesstoken as string;
+
+            if (!accessToken) {
+                res.status(StatusCode.SERVER_AUTH_ERROR).json({
+                    message: 'token is missing',
+                });
+            }
+
+            const isAccessTokenExpired = isTokenExpried(accessToken as string);
+            if (isAccessTokenExpired) {
+                // if access token is expired
+                // decode access token
+                const data = jwt.decode(accessToken) as JwtPayload;
+
+                // get refresh token
+                const ip = req.socket.remoteAddress;
+                const refreshToken = await RedisAdapter.get(`RFT-${data.user_id}-${ip}`);
+                const isRefreshTokenExpired = isTokenExpried(refreshToken as string);
+
+                // check refresh token expired time
+                if (isRefreshTokenExpired) {
+                    await RedisAdapter.delete(`RFT-${data.user_id}-${ip}`);
+
+                    req.headers.accessToken = null;
+                    
+                    res.status(StatusCode.VERIFY_FAILED).json({
+                        message: 'user needs to log in again!',
+                    });
+                } else {
+                    // create new access token
+                    const newAccessToken = await Token.accessToken({
+                        user_id: data.user_id,
+                        user_name: data.user_name,
+                        remaining_lessions: data.remaining_lessions,
+                    });
+
+                    req.headers.accessToken = newAccessToken;
+
+                    next();
+                }
+            } else {
+                next();
+            }
+        } catch (error) {
+            next(error);
+        }
+    }
+}
---
+++ src/config/token.ts
@@ -0,0 +1,51 @@
+import { IUserResponse } from '@common/user/User.model';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import { ACCESSTOKEN_KEY, REFRESHTOKEN_KEY } from './environment';
+import { IToken } from './token.interface';
+
+export class Token {
+    public static async getToken(data: IUserResponse): Promise<IToken> {
+        const accessToken = await Token.accessToken(data);
+        const refreshToken = await Token.refreshToken(data);
+
+        return {
+            accessToken,
+            refreshToken,
+        };
+    }
+
+    public static async accessToken(data: IUserResponse) {
+        return jwt.sign(
+            {
+                user_id: data.user_id,
+                user_name: data.user_name,
+                remaining_lessions: data.remaining_lessions,
+            },
+            ACCESSTOKEN_KEY,
+            {
+                expiresIn: '120s', // 120 seconds
+            },
+        );
+    }
+
+    public static async refreshToken(data: IUserResponse) {
+        return jwt.sign(
+            {
+                user_id: data.user_id,
+                user_name: data.user_name,
+                remaining_lessions: data.remaining_lessions,
+            },
+            REFRESHTOKEN_KEY,
+            {
+                expiresIn: '1d', // 1 day
+            },
+        );
+    }
+}
+
+export const isTokenExpried = (token: string) => {
+    const decodeToken = jwt.decode(token) as JwtPayload;
+    const expirationTime = decodeToken.exp * 1000;
+    const currentTime = Date.now().valueOf() / 1000;
+    return expirationTime <= currentTime;
+};
